53bec5317e7d50ca9ef7107d6cb8a01507c8d84b
Gordon <godsonnchy@gmail.com>
2020-08-07 11:42:23 +0300

    make some changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from tensorflow.keras.models import Sequential, save_model, load_model
 from tensorflow.keras.optimizers import Adam
@@ -10,30 +11,43 @@ model.compile(loss=sparse_categorical_crossentropy,
               optimizer=Adam(),
               metrics=['accuracy'])
 
-
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 classes=['healthy_tomato','healthy_wheat','unhealthy_tomato','unhealthy_wheat']
 
 # print(model)
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
-@app.route('/predict')
+@app.route('/predict', methods=['GET', 'POST'])
 def prediction():
-    # if there is any validation to be carried out here
-    file_ = tf.keras.utils.get_file('D:/images/cropImage', 'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz')
-    img = tf.keras.preprocessing.image.load_img(file_, target_size=[224, 224])
-    x = tf.keras.preprocessing.image.img_to_array(img)
-    x = tf.keras.applications.mobilenet.preprocess_input(
-    x[tf.newaxis,...])
 
-    y_prob = model.predict(x) 
-    y_classes = y_prob.argmax(axis=-1)
-    res=classes[y_classes[0]]
-    return jsonify(res)
+    if request.method == 'POST':
+        if request.files:
+            file = request.files['image']
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+            path = UPLOAD_FOLDER + '/' + file.filename
+
+            lyric_file = open(path, 'rt')
+            
+            # if there is any validation to be carried out here
+            file_ = path
+
+            img = tf.keras.preprocessing.image.load_img(file_, target_size=[224, 224])
+            x = tf.keras.preprocessing.image.img_to_array(img)
+            x = tf.keras.applications.mobilenet.preprocess_input(
+            x[tf.newaxis,...])
+
+            y_prob = model.predict(x) 
+            y_classes = y_prob.argmax(axis=-1)
+            res=classes[y_classes[0]]
+            return jsonify(res)
 
 
 if __name__ == '__main__':
